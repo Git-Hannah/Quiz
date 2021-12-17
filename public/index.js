a1dcb557d2cda39a5bfa02f2a595fa8543ff8781
Hannah <hannahsenft@gmx.net>
2021-12-17 10:34:47 +0100

    add changes

--- public/index.js
+++ public/index.js
@@ -1,10 +1,12 @@
 console.log("Hello World!");
 console.log(document.location);
 
-button.addEventListener("click", ev => {
-	console.log({ name: "Hannah" });
-});
+// document.addEventListener("click", ev => {
+// 	console.log({ name: "Hannah" });
+// });
 
+
+//Button Answer Showing
 const button = document.querySelector("button");
 console.log(button);
 
@@ -16,4 +18,18 @@ const answerShowing = "qcard-answer--showing";
 
 button.addEventListener("click", ev => {
     answer.classList.toggle(answerShowing);
+});
+
+//Navbar Toggle
+const footer = document.querySelector(".footer");
+console.log(footer);
+
+const navbar = document.querySelector(".navbar");
+console.log(navbar);
+
+//const navbarSelected = "footer--clicked";
+const navbarShowing = "navbar--showing";
+
+footer.addEventListener("click", ev => {
+    navbar.classList.toggle(navbarShowing);
 });
